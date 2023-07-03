cc746e847d36e4b0b78cc5f2f0916ae0fed7b420
JosuePimentel <josue.farias.pimentel@gmail.com>
2023-07-02 22:28:50 -0300

    new repository

---
+++ javascript.js
@@ -0,0 +1,11 @@
+const DOM = ele => document.querySelector(ele)
+
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+        setTimeout(() => elemento.innerHTML += letra, 75 * i);
+    });
+}
+
+typeWriter(DOM("section.main > pre"))
