feb452afe78c981c1ab499a82778a69d91011334
JosuePimentel <josue.farias.pimentel@gmail.com>
2023-07-12 20:19:57 -0300

    section about done

--- javascript.js
+++ javascript.js
@@ -14,10 +14,25 @@ const upAside = document.querySelector('aside')
 setInterval(() => {
     if (window.pageYOffset >= 60) upAside.classList.add('show')
     else upAside.classList.remove('show')
+    console.log(window.innerHeight)
+
+    if(window.pageYOffset >= (document.querySelector('#about').offsetTop - ((window.innerHeight * 3) / 4))) {
+        document.querySelector('span#id_1 > span#top > p').classList.add('show')
+        document.querySelector('span#id_1 > span#bottom > a').classList.add('show')
+
+        document.querySelector('span#id_2 > span#top > div').classList.add('show')
+        document.querySelector('span#id_2 > span#bottom > div').classList.add('show')
+    } else {
+        document.querySelector('span#id_1 > span#top > p').classList.remove('show')
+        document.querySelector('span#id_1 > span#bottom > a').classList.remove('show')
+
+        document.querySelector('span#id_2 > span#top > div').classList.remove('show')
+        document.querySelector('span#id_2 > span#bottom > div').classList.remove('show')
+    }
 }, 200)
 upAside.addEventListener('click', () => scrolar(0))
 
-
+console.log(document.querySelector('span#id_1').offsetTop)
 //Scroll smooth
 const sect = document.querySelectorAll('.menu > ul > li > a')
 sect.forEach(ele => {
